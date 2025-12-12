83257860dc9240481ebe8f78e9cc4dbfa355f6e0
Leo-Azouu <104171733+Leo-Azouu@users.noreply.github.com>
2025-12-12 15:14:07 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,43 +1,45 @@
 int main(int argc, char *argv[]) {
 
-    double r = 0;
+    // car : 1 opérande
+    if (argc == 3 && strcmp(argv[1], "car") == 0) {
+        double a = atof(argv[2]);
+        printf("%f\n", _car(a));
+        return 0;
+    }
 
-    /* cas add / sub / mul / div */
+    // add/sub/mul/div : 2 opérandes
     if (argc == 4) {
         char* op = argv[1];
-        char* a = argv[2];
-        char* b = argv[3];
+        double a = atof(argv[2]);
+        double b = atof(argv[3]);
+        double r = 0;
 
         if (strcmp(op, "add") == 0) {
-            r = _add(atof(a), atof(b));
-            printf("%f", r);
+            r = _add(a, b);
         }
         else if (strcmp(op, "sub") == 0) {
-            r = _sub(atof(a), atof(b));
-            printf("%f", r);
+            r = _sub(a, b);
         }
         else if (strcmp(op, "mul") == 0) {
-            r = _mul(atof(a), atof(b));
-            printf("%f", r);
+            r = _mul(a, b);
         }
         else if (strcmp(op, "div") == 0) {
-            r = _div(atof(a), atof(b));
-            printf("%f", r);
+            r = _div(a, b);
         }
         else {
-            printf("Erreur de parametres");
+            fprintf(stderr, "Erreur: operation inconnue (add/sub/mul/div/car)\n");
+            return 1;
         }
+
+        printf("%f\n", r);
+        return 0;
     }
 
-    /* cas car */
-    else if (argc == 3 && strcmp(argv[1], "car") == 0) {
-        double a = atof(argv[2]);
-        printf("%f", _car(a));
-    }
-
-    else {
-        printf("Erreur de parametres");
-    }
-
-    return 0;
+    fprintf(stderr, "Usage:\n");
+    fprintf(stderr, "  %s add <a> <b>\n", argv[0]);
+    fprintf(stderr, "  %s sub <a> <b>\n", argv[0]);
+    fprintf(stderr, "  %s mul <a> <b>\n", argv[0]);
+    fprintf(stderr, "  %s div <a> <b>\n", argv[0]);
+    fprintf(stderr, "  %s car <a>\n", argv[0]);
+    return 1;
 }
